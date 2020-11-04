d1281a1701775f9326039a98d70b86bcb859ac13
Lauren Harmon <harmon.lauren@hotmail.com>
2020-11-03 19:14:24 -0600

    why won't this work

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -4,6 +4,7 @@ export class Satellite {
     type: string;
     operational: boolean;
     launchDate: string;
+
     constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean) {
         this.name = name;
         this.type = type;
@@ -11,4 +12,14 @@ export class Satellite {
         this.orbitType = orbitType;
         this.operational = operational;
     }
+
+    shouldShowWarning(): boolean {
+        let check: string = "SPACE DEBRIS";
+        if(this.type.toLowerCase() !== check.toLowerCase()) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
 }
